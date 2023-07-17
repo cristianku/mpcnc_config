3460939ee4e5a8314a6682dd36585b3a8bf4cdd1
Cristian Zantedeschi <c.zantedeschi@gmail.com>
2023-07-17 23:13:43 +0200

    c

--- flash.sh
+++
@@ -1,8 +0,0 @@
-sudo service klipper stop
-cd ~/klipper
-git pull
-make clean
-make
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_lpc1769_2240001780A037AFC8E4BF5BC22000F5-if00 btt-skr-turbo-v1.4
-sudo service klipper start
-
